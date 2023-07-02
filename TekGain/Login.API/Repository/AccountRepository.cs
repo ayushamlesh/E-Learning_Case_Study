08937e0bb7ec3e3e12e23dbae1d34f2a4a620853
ayushamleh <ayushamlesh@gmail.com>
2023-07-02 16:29:58 +0530

    tek.dal

--- TekGain/Login.API/Repository/AccountRepository.cs
+++ TekGain/Login.API/Repository/AccountRepository.cs
@@ -30,7 +30,6 @@ namespace Login.API.Repository
 
         public async Task<IdentityResult> SignUp(SignUp signUpObj)
         {
-            // Check if the implementation is correct for SignUp in the repository while valid input
             // You can add your implementation or checks here
 
             // Check if the email already exists
@@ -107,55 +106,7 @@ namespace Login.API.Repository
               return tokenString;
           }
 
-        /*
-        public async Task<string> SignIn(SignIn signInObj)
-        {
-            var user = await _userManager.FindByEmailAsync(signInObj.Email);
-            if (user == null || !await _userManager.CheckPasswordAsync(user, signInObj.Password))
-            {
-                _logger.LogWarning($"{DateTime.Now} WAR: Sign failed : {signInObj.Email}");
-                return "Incorrect Email/Password";
-            }
-
-            var roles = await _userManager.GetRolesAsync(user);
-            var claims = new List<Claim>
-    {
-        new Claim(ClaimTypes.Email, user.Email),
-    };
-
-            foreach (var role in roles)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role));
-            }
-
-            var tokenSecret = _configuration["JWT:Secret"];
-            var tokenExpirationDays = Convert.ToInt32(_configuration["JWT:TokenExpirationDays"]);
-
-            var token = GenerateJwtToken(_configuration["JWT:ValidIssuer"], _configuration["JWT:ValidAudience"], tokenSecret, tokenExpirationDays, claims);
-
-            _logger.LogInformation($"{DateTime.Now} INFO: Sign success {signInObj.Email}");
-            return token;
-        }
-
-        private string GenerateJwtToken(string issuer, string audience, string tokenSecret, int tokenExpirationDays, IEnumerable<Claim> claims)
-        {
-            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSecret));
-            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
-
-            var expires = DateTime.Now.AddDays(tokenExpirationDays);
-
-            var token = new JwtSecurityToken(
-                issuer: issuer,
-                audience: audience,
-                claims: claims,
-                expires: expires,
-                signingCredentials: signingCredentials
-            );
-
-            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-            return tokenString;
-        }
-       */
+      
 
     }
 }
--- TekGain/TekGain.DAL/Entities/SignUp.cs
+++ TekGain/TekGain.DAL/Entities/SignUp.cs
@@ -6,20 +6,20 @@ namespace TekGain.DAL.Entities
     public class SignUp
     {
         // Implement the properties here
-        [Required(ErrorMessage = "First name is required")]
+        [Required(ErrorMessage = "First name required")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last name is required")]
+        [Required(ErrorMessage = "Last name required")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Email address is required")]
+        [Required(ErrorMessage = "Email address required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Password required")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Confirm password is required")]
+        [Required(ErrorMessage = "Confirm password required")]
         [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; }
     }
