2777c6d2cffb67066f9852160a8fbfa072a1d16a
AyushAmlesh <ayushamlesh@gmail.com>
2023-07-13 16:15:15 +0530

    Update FunctionalTest.cs

--- TekGain/Course.API/FunctionalTest.cs
+++ TekGain/Course.API/FunctionalTest.cs
@@ -1,4 +1,4 @@
-﻿using Course.API.Repository;
+using Course.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -19,9 +19,18 @@ namespace Course.API
     {
         // NOTE :
         // 1. SHOULD NOT CHANGE THE TESTCASE NAME
+        private Mock<ICourseRepository> _courseRepositoryMock;
+        private ILogger<CourseRepository> _loggerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _courseRepositoryMock = new Mock<ICourseRepository>();
+            _loggerMock = Mock.Of<ILogger<CourseRepository>>();
+        }
         // 2. iMPLEMENT THE TESTCASE AS PER THE REQUIREMENT MENTIONED THE EACH TESTCAESE
 
-
+       
 
         [Test, Order(1)]
         public void Test1_TDD_Invoke_AddCourse_Method_ForValid()
@@ -40,7 +49,19 @@ namespace Course.API
             // TEST THE COURSE REPOSITORY 'GETCOURSEBYID' PROCESS TO SEE WHETHER IT SUCCEEDS OR FAILS
             // IMPLEMENTATION IS ACCURATE OR NOT FOR VALID CASES
             // Arrange
-         
+            var course = new TekGain.DAL.Entities.Course { Id = 1 };
+
+            _courseRepositoryMock.Setup(repo => repo.GetCourseById(1)).Returns(course);
+            var courseController = new CourseController(_courseRepositoryMock.Object);
+
+            // Act
+            var result = courseController.GetCourseById(1);
+
+            // Assert
+            Assert.IsTrue(result is OkObjectResult);
+            Assert.AreEqual(course, ((OkObjectResult)result).Value);
+
+
 
 
         }
@@ -51,7 +72,14 @@ namespace Course.API
             // REQUIREMENT :
             // TEST THE COURSE REPOSITORY 'GETCOURSEBYID' PROCESS TO SEE WHETHER IT SUCCEEDS OR FAILS
             // IMPLEMENTATION IS ACCURATE OR NOT FOR VALID CASES
-          
+          _courseRepositoryMock.Setup(repo => repo.GetCourseById(111)).Throws(new ServiceException("Invalid Course Id"));
+            var courseController = new CourseController(_courseRepositoryMock.Object);
+
+            // Act
+            var result = courseController.GetCourseById(111);
+
+            // Assert
+            Assert.IsTrue(result is BadRequestObjectResult);
 
 
 
@@ -65,6 +93,17 @@ namespace Course.API
             // TEST THE COURSE REPOSITORY 'GETRATING' PROCESS TO SEE WHETHER IT SUCCEEDS OR FAILS
             // IMPLEMENTATION IS ACCURATE OR NOT FOR VALID CASES
             // Arrange
+            int courseId = 1;
+            var rating = 4.5;
+            _courseRepositoryMock.Setup(repo => repo.GetRating(courseId)).Returns(rating);
+            var courseController = new CourseController(_courseRepositoryMock.Object);
+
+            // Act
+            var result = courseController.GetRating(courseId);
+
+            // Assert
+            Assert.IsTrue(result is OkObjectResult);
+            Assert.AreEqual(rating, ((OkObjectResult)result).Value);
            
         }
 
